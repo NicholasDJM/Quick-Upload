72d6b3c6bd469aa3426827c58b3dc8775eec5286
NicholasDJM <11604027+NicholasDJM@users.noreply.github.com>
2025-01-06 12:08:48 -0500

    first commit

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Quick Upload ![License AGPL 3.0 or later](https://img.shields.io/github/license/NicholasDJM/Quick%20Upload)
+
+Quickly upload files from one computer to another.
+
+<picture>
+	<img src="./screenshotMobile.png" alt="Screen shot of the program on a phone." height="650"/>
+<picture>
+
+
+> [!CAUTION]
+> This program has ZERO security. Do not use over the internet, don't expose the port 3000 while program is in use.
+
+1. First install dependencies: `npm i`
+2. Then run: `node server.js`. Now this computer will receive files from other computers.
+3. Now, open your terminal. If on Linux, type `ifconfig`, to find your computer's local IP address. If on Windows, type `ipconfig`.
+4. On your other computer or phone: open a web browser, and navigate to the IP address found above.
+5. Finally, upload your files! If the host computer is on Windows, it will automatically open Explorer to the uploaded file. Files are saved in `./upload`.
---
+++ server.js
@@ -0,0 +1,106 @@
+/* AGPL 3.0 or later */
+import express from "express";
+import multer from "multer";
+import morgan from "morgan";
+import ansis from "ansis";
+import { join } from "node:path";
+import { cwd, platform } from "node:process";
+import { exec } from "node:child_process"
+
+const uploadLocation = "uploads",
+	storage = multer.diskStorage({
+		destination: function (req, file, cb) {
+			cb(null, uploadLocation+"/")
+		},
+		filename: function (req, file, cb) {
+			const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9),
+				filename = file.originalname.split("."),
+				sansExt = [...filename],
+				ext = [...filename].at(-1)
+			sansExt.pop()
+			console.dir(file)
+			cb(null, sansExt + '-' + uniqueSuffix + "." + ext )
+		}
+	}),
+	upload = multer({ storage }),
+	app = express(),
+	html = `<!doctype html>
+	<html lang="en">
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Quick Upload</title>
+		<style>
+			:root{
+				--green:green;
+				color-scheme:light dark
+			}
+			body{
+				height:100%;
+				margin:0
+			}
+			@media(prefers-color-scheme:dark){
+				:root{
+					--green:lightgreen
+				}
+			}
+			*{
+				box-sizing:border-box
+			}
+			form{
+				height:100dvb;
+				margin:0;
+				display:grid;
+				grid-template-rows:repeat(3, auto);
+				gap:.5rem;
+				padding:9px;
+				place-content:center center
+			}
+		</style>
+	<head>
+	<body>
+		<form action="/" method="post" enctype="multipart/form-data">
+			<p style="color:`,
+	html2=`</p>
+			<input type="file" name="file">
+			<button>Upload</button>
+		</form>
+	</body>
+</html>`
+
+app.use(morgan(function(tokens,req,res){
+	let status = tokens.status(req,res)
+	
+	switch (status[0]) {
+		case "4":
+		case "5":
+			status = ansis.red.bold(status)
+			break;
+		case "2":
+			status = ansis.green.bold(status)
+			break;
+	}
+	return [
+		ansis.yellow.bold(tokens.method(req,res)),
+		ansis.blue.bold(tokens.url(req,res)),
+		status
+	].join(" ")
+}))
+app.get("/", (req, res) => {
+	res.send(html+"unset\">"+html2)
+})
+
+app.post("/", upload.single("file"), (req, res) => {
+	if (!req.file) {
+		res.status(400)
+			.send(html+`red">Please select a file first.`+html2)
+	} else {
+		res.send(html+`var(--green)">File uploaded!`+html2)
+		if (platform === "win32") exec(`explorer.exe /select,"${join(cwd(),uploadLocation,req.file.filename)}"`,(err,output,input)=>{})
+		
+	}
+})
+
+app.listen(3000, ()=>{
+	console.info(`Listening on port ${ansis.yellow.bold(3000)}. Linux: Check ifconfig command for local address, Windows: Check ipconfig.`)
+	console.error(ansis.red.bold("DANGER:") + " Do not use expose this program to the internet, there is ZERO security.")
+})
